b30ce485eacc1e9dfe151a4c41b9c8818a72595c
landwarrior <tech_land_equilibrium@outlook.jp>
2020-07-08 23:37:21 +0900

    とりあえずコミットしておく

--- src/main/java/sample/web/IndexController.java
+++ src/main/java/sample/web/IndexController.java
@@ -1,11 +1,12 @@
 package sample.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-	@RequestMapping("/index")
+
+	@GetMapping("/index")
 	public String index() {
 		return "index";
 	}
