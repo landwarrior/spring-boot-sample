08fdbd6e3b912f46f63630df80727b7850eaa1bf
landwarrior <tech_land_equilibrium@outlook.jp>
2020-07-11 13:34:16 +0900

    いったんDBとセキュリティは無視して進める

--- src/main/java/sample/web/IndexController.java
+++ src/main/java/sample/web/IndexController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-	@GetMapping("/index")
+	@GetMapping("/")
 	public String index() {
-		return "index";
+		return "redirect:/hello";
 	}
 }
