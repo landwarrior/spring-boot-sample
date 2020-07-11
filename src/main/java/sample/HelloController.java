aa86dee019caedbfa71b11880e0dc3b5a2e38b99
landwarrior <tech_land_equilibrium@outlook.jp>
2020-07-11 14:36:15 +0900

    まだ簡単なことしか書いてないけどコミットしておく

--- src/main/java/sample/HelloController.java
+++ src/main/java/sample/HelloController.java
@@ -1,13 +1,29 @@
 package sample;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * RestController アノテーションがついているので thymeleaf は使わない
+ * 返している文字列がそのまま http のボディに入るんじゃないかな
+ */
 @RestController
 public class HelloController {
 
+    /**
+     * コンテキストパスそのまま
+     */
     @GetMapping("/hello")
     public String hello() {
         return "Hello, World!";
+    }
+
+    /**
+     * コンテキストパスでパラメータを受け付ける書き方
+     */
+    @GetMapping("/hello/{num}")
+    public String variableHello(@PathVariable int num) {
+        return "Hello, Variable! " + Integer.toString(num);
     }
 }
--- src/main/java/sample/web/IndexController.java
+++ src/main/java/sample/web/IndexController.java
@@ -1,6 +1,7 @@
 package sample.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,6 +9,20 @@ public class IndexController {
 
 	@GetMapping("/")
 	public String index() {
+        // とりあえずリダイレクトで RestController に飛ばしておく
 		return "redirect:/hello";
 	}
+
+    /**
+     * ここでは thymeleaf を使って Web ページを表示させてみる
+     *
+     * @param model ビューに渡すデータを格納するオブジェクト
+     */
+    @GetMapping("/html")
+    public String htmlIndex(Model model) {
+        // モデルに対して値をセットしてみる
+        model.addAttribute("test", "テスト表示");
+        // resources/templates ディレクトリ配下の html ファイルが呼び出される
+        return "index";
+    }
 }
